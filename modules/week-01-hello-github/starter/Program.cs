370b0bf07d3dc0b70e965fc740a38f491ef60f32
Eric-Rog <97482726+Eric-Rog@users.noreply.github.com>
2026-01-12 20:02:19 -0800

    Output 'Hello, GitHub!' to the console

--- modules/week-01-hello-github/starter/Program.cs
+++ modules/week-01-hello-github/starter/Program.cs
@@ -1,9 +1,21 @@
+/**************************************************************
+* Course: DEV 110
+* Term: Winter 2026
+*
+* Programmer: Eric Rogers
+* Assignment: Hello World!
+*
+* Description:
+* Print 'Hello World' into the console
+*
+**************************************************************/
+
 namespace HelloGitHub;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        // TODO: Write code that outputs "Hello, GitHub!" to the console
+        Console.WriteLine("Hello, GitHub!");
     }
 }
