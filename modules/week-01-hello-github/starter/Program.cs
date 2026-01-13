3ea67a725b6ca5a44787dd562c4a93431a28ac97
AlexHoang <hoangalexander@gmail.com>
2026-01-12 20:50:53 -0800

    Validate assignment testing

--- modules/week-01-hello-github/starter/Program.cs
+++ modules/week-01-hello-github/starter/Program.cs
@@ -3,7 +3,7 @@ namespace HelloGitHub;
 public class Program
 {
     public static void Main(string[] args)
-    {
+    {Console.WriteLine("Hello, GitHub!")
         // TODO: Write code that outputs "Hello, GitHub!" to the console
     }
 }
